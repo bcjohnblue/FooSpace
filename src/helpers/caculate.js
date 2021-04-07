e7893749fa79f11093d9b15694006bafd8fb2c39
bcjohn <bcjohnblue@gmail.com>
2021-04-07 23:32:30 +0800

    Add isProductExist

--- src/helpers/caculate.js
+++ src/helpers/caculate.js
@@ -1,6 +1,6 @@
 const calculate = (products) => {
   return products.reduce(
-    (totalPrice, product) => totalPrice + (product.finalPrice || product.price),
+    (totalPrice, product) => totalPrice + (product.finalPrice || product.price || 0),
     0
   );
 };
--- src/helpers/normalize.js
+++ src/helpers/normalize.js
@@ -1,6 +1,8 @@
 import database from '../database.json';
 const products = database.products;
 
+const getIsProductExist = (product) => product && product.name;
+
 const normalize = (productIDs) => {
   const selectedProducts = [];
   const productCounts = {};
@@ -10,12 +12,15 @@ const normalize = (productIDs) => {
       ? productCounts[productID] + 1
       : 1;
 
-    selectedProducts.push({
-      id: index + 1,
-      productID,
-      ...products[productID],
-      count: productCounts[productID]
-    });
+    const isProductExist = getIsProductExist(products[productID]);
+    if (isProductExist) {
+      selectedProducts.push({
+        id: index + 1,
+        productID,
+        ...products[productID],
+        count: productCounts[productID]
+      });
+    }
   });
 
   return selectedProducts;
